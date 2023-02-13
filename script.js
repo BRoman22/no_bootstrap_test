04f2885d4f2a58848a970dfecb824bdb05ed7844
Roman <Rbondarev22@gmail.com>
2023-02-13 20:29:04 +0530

    gg

--- script.js
+++ script.js
@@ -1,32 +1,94 @@
-const loginBtn = document.querySelector('.login_button');
-const loginForm = document.querySelector('.popup_form');
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelectorAll('.popup_area, .popup_close');
-const showPassword = document.querySelector('.popup_show_password');
-const loginInputs = document.querySelectorAll('.popup_form_input');
 const body = document.querySelector('body');
+const popup = document.querySelectorAll('.popup');
+const form = document.querySelectorAll('.popup_form');
+const loginButton = document.querySelectorAll('.login_button, .popup_login_button');
+const registrationButton = document.querySelector('.popup_registration_button');
+const popupClose = document.querySelectorAll('.popup_area, .popup_close');
+const inputs = document.querySelectorAll('.popup_form_input');
+const showPassword = document.querySelectorAll('.popup_show_password');
+const toggleIconPassword = document.querySelectorAll('.popup_show_password_background')
+const email = [inputs[1], inputs[3]];
 
 
-loginBtn.onclick = function () {
-    popup.classList.add('open');
+email.forEach(e => e.addEventListener('input', function () {
+    let gg = this.value;
+    (gg.length < 6) ? e.classList.add('error') : e.classList.remove('error');
+}));
+
+
+loginButton.forEach(e => e.onclick = () => {
+    popup[1].classList.add('open');
+    popup[0].classList.remove('open');
     body.classList.add('scroll_lock');
-}
+});
 
-popupClose.forEach(close => close.onclick = function () {
-    popup.classList.remove('open');
+registrationButton.onclick = () => {
+    popup[1].classList.remove('open');
+    popup[0].classList.add('open');
+};
+
+popupClose.forEach(e => e.onclick = () => {
+    popup.forEach(popup => popup.classList.remove('open'));
     body.classList.remove('scroll_lock');
-})
+});
 
-showPassword.onclick = () => { (loginInputs[1].type === 'password') ? loginInputs[1].type = 'text' : loginInputs[1].type = 'password' };
 
-loginForm.onsubmit = function () {
-    let submitForm;
-
-    loginInputs.forEach(input => checkSpace(input));
-    loginInputs.forEach(input => input.placeholder = 'Обязательное поле');
-
-    loginInputs.forEach(input => (input.value === '') ? submitForm = false : '');
-    return submitForm;
+function switchIcon(icon, input) {
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon[1].classList.remove('unactive');
+        icon[0].classList.add('unactive');
+    } else {
+        input.type = 'password';
+        icon[1].classList.add('unactive');
+        icon[0].classList.remove('unactive');
+    }
 }
 
-function checkSpace(input) { (input.value === '') ? input.classList.add('error') : input.classList.remove('error') };
+showPassword[0].onclick = () => {
+    let toggle = [toggleIconPassword[0], toggleIconPassword[1]];
+    let inputPassword = inputs[2];
+
+    switchIcon(toggle, inputPassword);
+}
+
+showPassword[1].onclick = () => {
+    let toggle = [toggleIconPassword[2], toggleIconPassword[3]];
+    let inputPassword = inputs[4];
+
+    switchIcon(toggle, inputPassword);
+}
+
+
+function checkSpace(e) {
+    if (e.value === '') {
+        e.classList.add('error');
+        e.placeholder = 'Заполните поле';
+        event.preventDefault();
+    } else {
+        e.classList.remove('error');
+    }
+};
+
+
+form[0].onsubmit = function () {
+    let input = [inputs[0], inputs[1], inputs[2]];
+    let mail = email[0];
+
+    input.forEach(e => checkSpace(e));
+    if (mail.value.length < 6) {
+        mail.classList.add('error');
+        event.preventDefault();
+    }
+};
+
+form[1].onsubmit = function () {
+    let input = [inputs[3], inputs[4]];
+    let mail = email[1];
+
+    input.forEach(e => checkSpace(e));
+    if (mail.value.length < 6) {
+        mail.classList.add('error');
+        event.preventDefault();
+    }
+};
